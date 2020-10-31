5cc098ed02d708926aabb3f9b5adc7fd021f60d6
HolmlundVD <71910342+HolmlundVD@users.noreply.github.com>
2020-10-30 21:35:17 -0500

    Add files via upload
    Loaded Files

---
+++ public_html/LogPage.js
@@ -0,0 +1,34 @@
+/**
+ * @author Vincent Holmlund
+ * @date 10/15/2020
+ */
+"use strict";
+var setupTable={
+   polus:{
+       expectedURL:"",
+       imageURL:"",
+       taskList:"polusTaskList.json"
+                 
+   },
+   miraHQ:{
+       expectedURL:"",
+       imageURL:"",
+       taskList:"miraTaskList.json"
+   },
+   theSkeld:{
+       expectedURL:"",
+       imageURL:"",
+       taskList:"skeldTaskList.json"
+   }
+};
+window.onload= function(){
+    document.getElementsByClassName("heading").innerHTML="does this test work";
+};
+document.getElementById("heading").onclick=function(){
+    
+};
+function setupPage( mapToUse=setupTable.theSkeld){
+};
+document.getElementById("dropbtn").onclick=function(){
+    document
+}    
